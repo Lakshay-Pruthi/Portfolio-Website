df0e5c185548edfd220263ebc985ea0a5d4fadfd
Lakshay Pruthi <lakshay47pruthi16@gmail.com>
2023-03-19 16:21:40 +0530

    navbar added

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,21 @@
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
         console.log(entry);
         entry.target.classList.toggle('show', entry.isIntersecting);
-        
+
     })
 })
 
 const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el)=> observer.observe(el));
+hiddenElements.forEach((el) => observer.observe(el));
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
